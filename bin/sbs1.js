ce60dbda31c1604507feb67609d9cab792a04186
John Wiseman <jwiseman@morseproject.com>
2013-05-23 11:09:35 -0700

    Added usage info to sbs1.js.

--- bin/sbs1.js
+++ bin/sbs1.js
@@ -1,6 +1,32 @@
-var argv = require('optimist').argv;
+// Sample program that prints aircraft coordinates.
+
+var usage = require('optimist')
+  .usage('Print aircraft positions\n\n' +
+         'Read SBS1 messages from a file:\n' +
+         '  $0 < <file>\n\n' +
+         'Connect to server that streams SBS1 messages:\n' +
+         '  $0 [--port <port>] <host>');
 var sbs1 = require('../index.js');
 var readline = require('readline');
+
+var argv = usage.argv;
+
+
+function print_msg(msg) {
+  if (msg.callsign) {
+    console.log(msg.hex_ident + ' (' + msg.callsign + '): ' +
+                msg.lat + ', ' + msg.lon);
+  } else {
+    console.log(msg.hex_ident + ': ' +
+                msg.lat + ', ' + msg.lon);
+  }
+}
+
+
+if (argv.help || argv.h) {
+  console.log(usage.help());
+  process.exit();
+}
 
 if (argv._.length > 0) {
   // Connect to server
@@ -12,7 +38,7 @@ if (argv._.length > 0) {
   });
   client.on('message', function(msg) {
     if (msg.lat !== undefined && msg.lat !== null) {
-      console.log(msg.lat + ', ' + msg.lon);
+      print_msg(msg);
     }
   });
 } else {
@@ -25,7 +51,7 @@ if (argv._.length > 0) {
   rl.on('line', function(line) {
     var msg = sbs1.parseSbs1Message(line);
     if (msg.lat !== undefined && msg.lat !== null) {
-      console.log(msg.lat + ', ' + msg.lon);
+      print_msg(msg);
     }
   });
 }
